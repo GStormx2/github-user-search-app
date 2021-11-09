030ab961914644c3b1801b79c07ac72978eeb3a9
Tanqir Masroor <tanqir.msarker@gmail.com>
2021-11-09 21:47:38 +0600

    fixed useState warning in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import sun from '../assets/sun.svg';
 import moon from '../assets/moon.svg';
 import { useUserData } from '../context/UserContext';
